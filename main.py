176142c64f284b036195ff2f6a791df060a7db7d
EADZblueWhale <138927937+EADZblueWhale@users.noreply.github.com>
2024-08-16 19:18:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import random
 
+
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sayi = int(input("Şifreniz kaç karekterden oluşsun istersiniz?"))
+
+
+password = ""
+for i in range(sayi):
+    password += random.chice(karakterler)
+
+
+print("Şifreniz:" password)
+
+
+if password=="admin":
+    print("bu şifreyi kullanamazsınız")
